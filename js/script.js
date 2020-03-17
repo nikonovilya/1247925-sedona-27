31719d66fe9db906d14d08c01e28d09c6c45c066
nikon <nikonowilya@gmail.com>
2020-03-17 10:57:48 +0300

    Ошибки кроссбраузерности.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let button = document.querySelector('.search__btn');
 let popup = document.querySelector('.search__modal-window');
-popup.classList.add("search__modal-window--closed");  
+popup.classList.add("search__modal-window--closed");
 button.addEventListener("click", function(evt) {
    evt.preventDefault();
    popup.classList.toggle("search__modal-window--closed")
