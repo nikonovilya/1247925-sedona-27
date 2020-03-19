58bbe99739a26fb49757970fd8d8f53b0995113a
nikon <nikonowilya@gmail.com>
2020-03-20 02:25:43 +0300

    Д17, исправление ошибок.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
-let button = document.querySelector('.search__btn');
-let popup = document.querySelector('.search__modal-window');
+const button = document.querySelector('.search__btn');
+const popup = document.querySelector('.search__modal-window');
+const form = popup.querySelector('.search__modal-form');
+const arrival = popup.querySelector('.modal-form__arrival-input');
+const departure = popup.querySelector('.modal-form__departure-input');
+
 popup.classList.add("search__modal-window--closed");
-button.addEventListener("click", function(evt) {
-   evt.preventDefault();
-   popup.classList.toggle("search__modal-window--closed")
+
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  if (popup.classList.contains("search__modal-window--closed")) {
+    popup.classList.remove("search__modal-window--closed");
+    arrival.focus();
+
+  } else {
+    popup.classList.add("search__modal-window--closed");
+    popup.classList.remove("search__modal-window--error");
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value) {
+    evt.preventDefault();
+    popup.classList.remove("search__modal-window--error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("search__modal-window--error");
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (!popup.classList.contains("search__modal-window--closed")) {
+      popup.classList.add("search__modal-window--closed");
+      popup.classList.remove("search__modal-window--error");
+    }
+  }
 });
